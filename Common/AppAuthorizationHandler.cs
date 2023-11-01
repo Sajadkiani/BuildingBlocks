c2c6aadc87597c3026adc2c3e6d2b88bb0729129
Sajad Kiani <sajadkiani@outlook.com>
2023-11-01 11:58:37 +0330

    add common proj required claims

---
+++ Common/AppAuthorizationHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Common;
+
+public sealed class RequiredClaimsAttribute : Attribute, IAuthorizationFilter
+{
+    private readonly string[] claimNames;
+
+    public RequiredClaimsAttribute(params string[] claimNames)
+    {
+        this.claimNames = claimNames;
+    }
+    
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!claimNames.All(claim => context.HttpContext.User.Claims.Any(clm => clm.Value == claim)))
+        {
+            context.Result = new UnauthorizedObjectResult(string.Empty);
+        }
+    }
+}
