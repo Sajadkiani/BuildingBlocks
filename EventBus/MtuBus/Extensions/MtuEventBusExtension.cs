b745f40b41cf77b9b0bd50a02ba009d9353bbf85
s.kiani <sa@7913882>
2025-11-26 09:31:09 +0330

    change in mtu extension method

--- EventBus/MtuBus/Extensions/MtuEventBusExtension.cs
+++ EventBus/MtuBus/Extensions/MtuEventBusExtension.cs
@@ -4,6 +4,7 @@ using EventBus.MtuBus.Tests;
 using EventBus.Services;
 using Identity.Infrastructure.MtuBus;
 using Identity.Infrastructure.MtuBus.Consumers;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
@@ -13,11 +14,38 @@ namespace EventBus.MtuBus.Extensions;
 
 public static class MtuEventBusExtension
 {
-    public static IServiceCollection AddMtuBus(this IServiceCollection services, IConfiguration configuration, string sectionName = "Rabbitmq")
+    public static IServiceCollection AddMtuBus(this IServiceCollection services, IConfiguration configuration,
+        string connectionStringSectionName, string sectionName = "Rabbitmq")
     {
-        services.Configure<MtuRabbitMqOptions>(configuration.GetSection(sectionName));
+        var connectionString = configuration.GetConnectionString(connectionStringSectionName);
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("connection string not found");
+        }
 
+        var mtuRabbitmq = configuration.GetSection(sectionName);
+        if (string.IsNullOrWhiteSpace(connectionStringSectionName))
+        {
+            throw new Exception("rabbitmq section not found");
+        }
+        
+        services.Configure<MtuRabbitMqOptions>(mtuRabbitmq);
+        AddMtuPublisher(services, connectionString);
+        AddMtuConsumer(services);
 
+        return services;
+    }
+
+    private static void AddMtuConsumer(IServiceCollection services)
+    {
+        services.AddScoped<IMtuConsumer, TestConsumer>();
+
+        services.AddHostedService<MtuBusHostedService>();
+    }
+
+    private static void AddMtuPublisher(IServiceCollection services, string dbConnectionString)
+    {
+        services.AddDbContext<EventLogDbContext>(opt => opt.UseSqlServer(dbConnectionString));
         services.AddSingleton<IMtuBusConnectionManager, MtuBusConnectionManager>();
         services.AddScoped<IDomainEventDispatcher, DomainEventDispatcher>();
         services.AddScoped<IIntegrationEventLogService, IntegrationEventLogService>();
@@ -28,11 +56,5 @@ public static class MtuEventBusExtension
 
             return IntegrationEventDispatcher.CreateAsync(options, logger).GetAwaiter().GetResult();
         });
-
-        services.AddScoped<IMtuConsumer, TestConsumer>();
-
-        services.AddHostedService<MtuBusHostedService>();
-
-        return services;
     }
 }
